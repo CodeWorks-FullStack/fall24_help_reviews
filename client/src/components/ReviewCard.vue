e6ede79674b0194b84061b873f60c237d0f18e49
Jeremy Fowler <jembyfowler@gmail.com>
2024-10-24 15:38:09 -0600

    showing your reviews on account page

--- client/src/components/ReviewCard.vue
+++ client/src/components/ReviewCard.vue
@@ -11,17 +11,28 @@ defineProps({
   <div class="bg-light shadow p-3">
     <div class="d-flex justify-content-between">
       <div>
-        <a v-if="review.imgUrl" :href="review.imgUrl" target="_blank" title="Picture of disgust">
+        <a :href="review.imgUrl" target="_blank" title="Picture of disgust">
           <i class="mdi mdi-camera text-success fs-3"></i>
         </a>
       </div>
       <h3 class="text-success">"{{ review.title }}"</h3>
-      <div class="d-flex gap-1">
-        <div class="text-end">
-          <p class="fw-bold mb-0">{{ review.reviewer.name }}</p>
-          <time>{{ review.createdAt.toLocaleDateString() }}</time>
+      <div>
+        <div v-if="review.reviewer" class="d-flex gap-1">
+          <div class="text-end d-none d-lg-block">
+            <p class="fw-bold mb-0">{{ review.reviewer.name }}</p>
+            <time>{{ review.createdAt.toLocaleDateString() }}</time>
+          </div>
+          <img :src="review.reviewer.picture" :alt="review.reviewer.name" class="reviewer-picture">
         </div>
-        <img :src="review.reviewer.picture" :alt="review.reviewer.name" class="reviewer-picture">
+        <div v-if="review.restaurant">
+          <RouterLink :to="{ name: 'Restaurant', params: { restaurantId: review.restaurant.id } }"
+            :title="`Go to the ${review.restaurant.name} page`">
+            <p class="text-success">
+              {{ review.restaurant.name }}
+              <i class="mdi mdi-arrow-right"></i>
+            </p>
+          </RouterLink>
+        </div>
       </div>
     </div>
     <p>{{ review.body }}</p>
--- client/src/pages/AccountPage.vue
+++ client/src/pages/AccountPage.vue
@@ -1,26 +1,28 @@
 <script setup>
 import { computed } from 'vue';
 import { AppState } from '../AppState.js';
+import ReviewCard from '@/components/ReviewCard.vue';
 
 const account = computed(() => AppState.account)
-
+const reviews = computed(() => AppState.myReviews)
 </script>
 
 <template>
-  <div class="about text-center">
-    <div v-if="account">
-      <h1>Welcome {{ account.name }}</h1>
-      <img class="rounded" :src="account.picture" alt="" />
-      <p>{{ account.email }}</p>
+  <div v-if="account" class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="text-center">Your Reviews</h1>
+      </div>
     </div>
-    <div v-else>
-      <h1>Loading... <i class="mdi mdi-loading mdi-spin"></i></h1>
+    <div class="row">
+      <div v-for="review in reviews" :key="review.id" class="col-10 m-auto mb-3">
+        <ReviewCard :review="review" />
+      </div>
     </div>
+  </div>
+  <div v-else class="container">
+    <h1>Loading... <i class="mdi mdi-loading mdi-spin"></i></h1>
   </div>
 </template>
 
-<style scoped lang="scss">
-img {
-  max-width: 100px;
-}
-</style>
+<style scoped lang="scss"></style>
